fe6b900e0469d4c8965a26d86ee009c4390e2414
Walter Chang <jackyhaha8@gmail.com>
2025-09-12 17:22:24 +0800

    feat: add foundry defi stablecoin

---
+++ advanced_foundry/foundry_defi_stablecoin/README.md
@@ -0,0 +1,14 @@
+1. Relative Stability: Anchored or Pegged -> $1.00
+   1. Chainlink Price Feed
+   2. Set a function to exchange ETH & BTC -> $$
+2. Stability Mechanism (Minting): Algorithmic (Decentralized)
+   1. People can only mint the stablecoin with enough collateral (coded)
+3. Colleteral: Exogenous (Crypto)
+   1. wETH
+   2. wBTC
+
+- caluculate health factor function
+- set health factor if debt is 0
+- Added a bunch of view function
+
+1. What are our invariants/properties?(-> what stateless and stateful fuzz testing is, and why it's so important to project like this potentially with a lot of money moving around.)
---
+++ advanced_foundry/foundry_defi_stablecoin/test/fuzz/OpenInvariantsTest.t.sol
@@ -0,0 +1,45 @@
+// Have our invariants aka properties
+
+// What are our invariants/properties?
+// 1. The total supply of DSC should be less than the total value of collateral
+// 2. Getter functions should never revert <- evergreen invariant
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract OpenInvariantsTest is StdInvariant, Test {
+    DeployDSC deployer;
+    DSCEngine dscEngine;
+    DecentralizedStableCoin dsc;
+    HelperConfig config;
+    address weth;
+    address wbtc;
+
+    function setUp() external {
+        deployer = new DeployDSC();
+        (dsc, dscEngine, config) = deployer.run();
+        (,, weth, wbtc,) = config.activeNetworkConfig();
+        targetContract(address(dscEngine));
+    }
+
+    function invariant_protocolMustHaveMoreValueThanTotalSupply() public view {
+        // get the value of all the collateral in the protocol
+        // compare it to all the debt (totalSupply of DSC)
+        uint256 totalSupply = dsc.totalSupply();
+        uint256 totalWethDeposited = IERC20(weth).balanceOf(address(dscEngine));
+        uint256 totalBtcDeposited = IERC20(wbtc).balanceOf(address(dscEngine));
+
+        uint256 wethValue = dscEngine.getUsdValue(weth, totalWethDeposited);
+        uint256 wbtcValue = dscEngine.getUsdValue(wbtc, totalBtcDeposited);
+
+        assert(wethValue + wbtcValue >= totalSupply);
+    }
+}
